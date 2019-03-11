ed000aad7680dd9cc824fb31190bfaeadeb0b012
RoshanBittu <48413087+RoshanBittu@users.noreply.github.com>
2019-03-11 19:04:10 +0530

    Add files via upload

---
+++ src/main/java/com/shoppingDemo/util/HibernateUtilConfig.java
@@ -0,0 +1,23 @@
+package com.shoppingDemo.util;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateUtilConfig {
+	
+	@Autowired
+	private EntityManagerFactory enityManagerFactory;
+	
+	@Bean
+	public SessionFactory getSessionFactory(){
+		if(enityManagerFactory.unwrap(SessionFactory.class)==null){
+			throw new NullPointerException("factory is not a hibernate factory");
+		}
+		return enityManagerFactory.unwrap(SessionFactory.class);
+	}
+}
