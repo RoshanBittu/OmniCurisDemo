597e73ceb90fc5b90ab871bc797650674009e130
RoshanBittu <48413087+RoshanBittu@users.noreply.github.com>
2019-03-11 19:03:09 +0530

    Add files via upload

---
+++ src/main/java/com/shoppingDemo/OmnicurisDemoApplication.java
@@ -0,0 +1,19 @@
+package com.shoppingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@RestController
+public class OmnicurisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnicurisDemoApplication.class, args);
+	}
+
+}
